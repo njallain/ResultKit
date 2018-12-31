1d642309f183c388a7d64c52ce83767268eaa6d6
Neil Allain <njallain@bluerope.org>
2018-12-31 14:07:58 -0600

    Initial Commit

---
+++ ResultKit/ResultKit.h
@@ -0,0 +1,19 @@
+//
+//  ResultKit.h
+//  ResultKit
+//
+//  Created by Neil Allain on 12/31/18.
+//  Copyright © 2018 Neil Allain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ResultKit.
+FOUNDATION_EXPORT double ResultKitVersionNumber;
+
+//! Project version string for ResultKit.
+FOUNDATION_EXPORT const unsigned char ResultKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ResultKit/PublicHeader.h>
+
+
